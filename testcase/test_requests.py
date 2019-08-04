329d457af0a7e7dbf31f79680429d483f7a1a89b
Natumeme <1466510866@qq.com>
2019-08-04 23:21:54 +0800

    add

--- testcase/test_requests.py
+++ testcase/test_requests.py
@@ -5,6 +5,7 @@ import requests
 import logging
 import pytest
 import json
+import jsonpath
 
 class TestRequests(object):
 	logging.basicConfig(level=logging.INFO)
@@ -17,12 +18,12 @@ class TestRequests(object):
 
 	def test_post(self):
 		r=requests.post(self.url,
-		                json={"a":1,"b":"string corntent"},
+		                json={"a":"1","b":"string corntent"},
 		                # headers={"a":"1","b":"b2"},
 		                # proxies={},
 		                verify=False
 		                )
-		logging.info(r.url)
+		# logging.info(r.url)
 		logging.info(r.text)
 		logging.info(json.dumps(r.json(),indent=2))
 
@@ -34,7 +35,17 @@ class TestRequests(object):
 		logging.info(r.text)
 
 	def test_xueqiu_quote(self):
-		pass
+		url = "https://stock.xueqiu.com/v5/stock/portfolio/stock/list.json?"
+		r = requests.get(url,
+		                 params={"category": "1"},
+		                 headers={'User-Agent': 'Xueqiu Android 11.19'},
+		                 cookies={"u": "6465710483", "xq_a_token": "f0c5a085040d507702cdeb99d51243451fc63d35"}
+		                 )
+		# logging.info(json.dumps(r.json(),ensure_ascii=False,indent=2))
+		assert r.json()["data"]["category"] == 1
+		assert r.json()["data"]["stocks"][0]["name"] == "招商银行"
+		assert jsonpath.jsonpath(r.json(),"$.data.stocks[?(@.symbol == 'SH600036')].name")[0] == "招商银行"
+		# assert_that(jsonpath.jsonpath(r.json(), "$.data.stocks[?(@.symbol == 'F006947')].name")[0],equal_to("华宝中短债债券B"), "比较上市代码与名字")
 
 
 
