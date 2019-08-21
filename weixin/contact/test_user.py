ee2d095fd006e7c359bd46c33ff4d7110ce87571
Natumeme <1466510866@qq.com>
2019-08-21 14:46:15 +0800

    update

--- weixin/contact/test_user.py
+++ weixin/contact/test_user.py
@@ -1,10 +1,11 @@
 #!usr/bin/env python
 #-*- coding:utf-8 -*-
 import json
-from datetime import time
+import time
 
 import requests
 import logging
+import pystache
 
 from weixin.contact.token import Weixin
 
@@ -34,6 +35,21 @@ class TestUser:
 		assert r["errcode"]==0
 
 	def test_create_by_template(self):
+		uid = "natume" + str(time.time())
+		mobile=str(time.time()).replace(".","")[0:11]
+		data=str(self.get_user({
+			"name":"natume",
+			"title":"校长",
+			"email":"1@1.com",
+			"mobile":mobile
+		}))
+		data=data.encode("UTF-8")
+		r = requests.post("https://qyapi.weixin.qq.com/cgi-bin/user/create",
+		                  params={"access_token": Weixin.get_token()},
+		                  data=data,
+		                  headers={"Content-Type":"application/json;charset=UTF-8"}).json()
+		logging.debug(r)
+		assert r["errcode"] == 0
 
 
 	def test_list(self):
@@ -44,3 +60,11 @@ class TestUser:
 		             params=params).json()
 		logging.debug(json.dumps(r,ensure_ascii=False,indent=2))
 		assert r["errcode"] == 0
+
+	def get_user(self,dict):
+		template="".join(open("user_create.json",encoding='utf-8').readlines())
+		#logging.debug(template)
+		return pystache.render(template,dict)
+
+	def test_get_user(self):
+		logging.debug(self.get_user({"name":"natume","title":"校长","email":"1@1.com"}))
