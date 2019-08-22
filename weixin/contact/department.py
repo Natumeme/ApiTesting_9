2047ce4ea9d8f03d8baeb3f6a4de33a3cd17eebb
Natumeme <1466510866@qq.com>
2019-08-22 16:21:11 +0800

    update

--- weixin/contact/department.py
+++ weixin/contact/department.py
@@ -1,2 +1,30 @@
 #!usr/bin/env python
 #-*- coding:utf-8 -*-
+
+import requests
+
+from weixin.contact.token import Weixin
+
+
+class Department:
+	def create(self,data):
+		return requests.post("https://qyapi.weixin.qq.com/cgi-bin/department/create",
+		                     params={"access_token": Weixin.get_token()},
+		                     json=data
+		                     ).json()
+
+	def get_department(self):
+		return requests.get("https://qyapi.weixin.qq.com/cgi-bin/department/list",
+		                 params={"access_token": Weixin.get_token()}
+		                 ).json()
+
+	def update_department(self,data):
+		return requests.post("https://qyapi.weixin.qq.com/cgi-bin/department/update",
+		                 params={"access_token": Weixin.get_token()},
+		                 json=data
+		                 ).json()
+
+	def delete_department(self):
+		return requests.get("https://qyapi.weixin.qq.com/cgi-bin/department/delete",
+		                 params={"access_token": Weixin.get_token(),"id":281}
+		                 ).json()
--- weixin/contact/test_department.py
+++ weixin/contact/test_department.py
@@ -1,17 +1,21 @@
 #!usr/bin/env python
 #-*- coding:utf-8 -*-
-import datetime
+
 import json
 import requests
-from weixin.contact.token import Weixin
 import logging
 import pytest
-
 from weixin.contact.utils import Utils
+from weixin.contact.department import Department
 
 
 class TestDepartment:
 	logging.basicConfig(level=logging.DEBUG)
+
+	@classmethod
+	def setup_class(cls):
+		cls.department = Department()
+
 	def test_create_depth(self,token):
 		#创建部门
 		parentid=278
@@ -29,26 +33,40 @@ class TestDepartment:
 			assert r["errorcode"]==0
 
 	@pytest.mark.parametrize("name",[
-		"妖精尾巴工会"
+		"妖精尾巴"
 	])
-	def test_create_order(self,name,token):
+	def test_create_order(self,name):
 		data = {
 			"name": name+Utils.udid(),
 			"parentid": 1,
 			"order": 1
 		}
-		r = requests.post("https://qyapi.weixin.qq.com/cgi-bin/department/create",
-		                  params={"access_token": token},
-		                  json=data
-		                  ).json()
+		r=self.department.create(data)
 
 		#解密
-		logging.debug(r)
+		logging.debug(json.dumps(r,indent=2,ensure_ascii=False))
 		assert r["errcode"]==0
 
-	def test_get(self,token):
+	def test_get(self):
 		#获取部门
-		r=requests.get("https://qyapi.weixin.qq.com/cgi-bin/department/list",
-		             params={"access_token":token}
-		             ).json()
+		r=self.department.get_department()
 		logging.debug(json.dumps(r,indent=2,ensure_ascii=False))
+		assert r["errcode"] == 0
+
+	def test_update(self):
+		#更新部门
+		data={
+		   "id": 281,
+		   "name": "妖精尾巴update",
+		   "parentid": 1,
+		   "order": 1
+		}
+		r=self.department.update_department(data)
+		logging.debug(json.dumps(r,indent=2,ensure_ascii=False))
+		assert r["errcode"] == 0
+
+	def test_delete(self):
+		#删除部门
+		r=self.department.delete_department()
+		logging.debug(json.dumps(r, indent=2, ensure_ascii=False))
+		assert r["errcode"] == 0
